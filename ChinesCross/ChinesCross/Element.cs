74ed286a99176ed78c08db49d5a6d121489bc4ea
gago55 <xgagik8@gmail.com>
2018-11-09 15:03:07 +0400

    created class Element which is group of dots (bool list)
    added `
        .property group
        .constructor which set group value

---
+++ ChinesCross/ChinesCross/Element.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChinesCross
+{
+    class Element
+    {
+        List<bool> group = new List<bool>();
+
+        public Element(int x)
+        {
+            for (int i = 0;  i < x;  i++)
+            {
+                group.Add(true);
+            }
+        }
+    }
+}
